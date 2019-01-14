c5b232f0b44667645df400951ddbe19817ab0ed4
Arnab Sen <azure3317@gmail.com>
2019-01-14 22:44:45 +1300

    [cosmetic] Code cleanup

--- scripts/main.js
+++ scripts/main.js
@@ -2,29 +2,23 @@ var canvas = document.querySelector("#mainCanvas");
 var ctx = canvas.getContext("2d");
 var clearButton = document.querySelector("#clear");
 var flagMouseDown = false;
-var strokePath = new Path2D();
 ctx.strokeStyle = ctx.fillStyle = "black";
 
 clearButton.addEventListener("click", e => {ctx.clearRect(0, 0, canvas.width, canvas.height);});
-
-canvas.addEventListener("click", e => 
-  ctx.fillRect(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop, 2, 2));
-  
+canvas.addEventListener("click", e => drawPixel(e));
 canvas.addEventListener("mousedown", e => {
   flagMouseDown = true; 
   ctx.beginPath();
   });
-  
 canvas.addEventListener("mouseup", e => flagMouseDown = false);
-
-canvas.addEventListener("mousemove", e => {
-  if (flagMouseDown) drawAtCurrentPos(e.clientX, e.clientY, 2, 2)
-  });
+canvas.addEventListener("mousemove", e => drawLine(e));
   
-function drawAtCurrentPos(x, y, color = null) {
-  if (color != null) ctx.strokeStyle = ctx.fillStyle = color;
-  x -= canvas.offsetLeft;
-  y -= canvas.offsetTop;
-  ctx.lineTo(x, y);
+function drawPixel(e) {
+  ctx.fillRect(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop, 2, 2);
+}
+  
+function drawLine(e) {
+  if (!flagMouseDown) return;
+  ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
   ctx.stroke();
 }
